4293cb2d9149382378d2fc1364bc62104bbf2694
ramoneirao <ramonneirao@gmail.com>
2025-09-23 00:40:04 -0300

    feat: inicia script para a analise exploratoria

---
+++ src/EDA.py
@@ -0,0 +1,16 @@
+"""
+Funções para Análise Exploratória de Dados (EDA)
+"""
+
+
+def plot_boxplot(df):
+    """
+    Plota um boxplot para todas as colunas numéricas do DataFrame.
+    """
+    pass
+
+
+def plot_histogram():
+    pass
+
+
