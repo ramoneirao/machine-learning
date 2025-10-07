d13f898719bac6e54f0f43a0a3d69c7207b95006
ramoneirao <ramonneirao@gmail.com>
2025-10-06 23:47:41 -0300

    feat: add função para remover outliers usando IQR

--- src/EDA.py
+++ src/EDA.py
@@ -93,3 +93,12 @@ def plot_histogram():
     pass
 
 
+def outliers_iqr(df,col):
+    Q1 = df[col].quantile(0.25)
+    Q3 = df[col].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
+    return df
+
